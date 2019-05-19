fd8bb59a96289bef2cb90da6a4d76a8eee845b79
Marin Kaçaj <m93_vkacaj@yahoo.com>
2019-05-19 18:26:13 +0200

    initial import

---
+++ advanced-squirrel-1/functions/shares.js
@@ -0,0 +1,13 @@
+exports.handler = function (event, context, callback) {
+	
+	let dataSeries = [
+		['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25],
+        ['data3', 30, 40, 50, 20, 10, 70, 30, 20, 40, 20]
+    ]
+    
+    callback(null, {
+    	statusCode: 200,
+    	body: JSON.stringify(dataSeries)
+    });
+}
